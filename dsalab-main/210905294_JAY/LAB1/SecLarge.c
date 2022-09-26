c28668eb5b7b5237e438e244d8bbc83fabad5cef
Jay <95573258+jayy-patell@users.noreply.github.com>
2022-09-26 15:19:01 +0530

    Update SecLarge.c

--- dsalab-main/210905294_JAY/LAB1/SecLarge.c
+++ dsalab-main/210905294_JAY/LAB1/SecLarge.c
@@ -1,66 +1,37 @@
-#include<stdio.h>
-
-int main(){
-    int a[10], n,i,lrg=0,lrg2=0;
-    printf("Enter size of array: ");
-    scanf("%d", &n);
-    printf("Enter elemetns: ");
-    for(i=0;i<n;i++){
-        scanf("%d", &a[i]);
-    }
-
-    for(i=0;i<n;i++){
-        if(lrg<a[i]){
-            lrg=a[i];
-            lrg2=lrg;
-        } else
-        if(a[i] > lrg2 && a[i] != lrg){
-            lrg2=a[i];
-        }
-    }
-
-    if (lrg2 == lrg)
-        printf("There is no second largest element\n");
-    else
-        printf("\nThe Second largest element in the array is: %d", lrg2);
-
-    return 0;
-}
-
-
-/*
 #include <stdio.h>
 
-int main(void) {
-    int x[10];
-    int i, n;
-    int first = x[0];
-    int second = x[0];
-
-    printf("Input the size of array :");
-    scanf("%d", &n);
-
-    printf("Input %d elements in the array :\n", n);
-    for (i = 0; i < n; i++) {
-        printf("x[%d]: ", i);
-        scanf("%d", &x[i]);
-    }
-
-    for (i = 0; i < n; ++i) {
-        if (first < x[i]) {
-            second = first;
-            first = x[i];
-        } else
-        if (x[i] > second && x[i] != first) {
-            second = x[i];
-        }
-    }
-
-    if (second == first)
-        printf("There is no second largest element\n");
-    else
-        printf("\nThe Second largest element in the array is: %d", second);
-
-    return 0;
+void secLarge(int a[], int n){
+	int i,lrg=0,lrg2=0;
+	for(i=0;i<n;i++){
+		if(lrg<a[i]){
+			lrg2=lrg;
+			lrg=a[i];
+		}
+		
+		else if (a[i] > lrg2 && a[i] != lrg) {
+            lrg2 = a[i];
+		}
+	}
+	if(lrg==lrg2){
+		printf("There is no second largest number.");
+	}else{
+		printf("The second largest number in the array is: %d", lrg2);
+	}
 }
-*/
+
+int main()
+{
+	int a[10],n,i;
+	printf("Enter size of array: ");
+	scanf("%d", &n);
+	if(n<2 || n>10){
+		printf("Invalid input.");
+	}
+	printf("Enter elements: ");
+	for(i=0;i<n;i++){
+		scanf("%d", &a[i]);
+	}
+	secLarge(a,n);
+	
+	return 0;
+}
